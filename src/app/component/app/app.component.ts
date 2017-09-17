fbbc4241f6a2c55ebeeee187d73dcf8545171059
heloisacarbone <heloisacarbone@gmail.com>
2017-09-17 13:30:08 -0300

    pegando sua location

--- src/app/component/app/app.component.ts
+++ src/app/component/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,9 +7,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   lat: number = 51.678418;
   lng: number = 7.809007;
+  
+  ngOnInit(): void {
+    console.log('aq');
+    if (window.navigator && window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition(
+          position => {
+            console.log(position)
+            this.lat = position.coords.latitude;
+            this.lng = position.coords.longitude;
+          }
+      );
+    }
+  }
   constructor(public router: Router) {}
 
 }
